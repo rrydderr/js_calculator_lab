715f2e63a3af1327dd80320c46936b0d927ec442
Ryder Rashid <ryderzane@Ryders-MacBook-Air-2.local>
2023-02-28 14:43:50 +0000

    first commit

---
+++ calculator.js
@@ -0,0 +1,45 @@
+const sum = function(a, b){
+    return a + b;
+};
+
+const subtract = function(a, b){
+    return a - b;
+};
+
+const multiply = function(a, b){
+    return a * b;
+};
+
+const divide = function (a, b){
+    return a / b;
+}
+
+const modulus = function (a, b){
+    return a % b;
+}
+
+const even = function (a){
+    if (a % 2 === 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const odd = function (a) {
+    if (a % 2 === 0){
+        return false;
+    } else {
+        return true;
+    }
+}
+
+module.exports = { 
+    sum, 
+    subtract, 
+    multiply, 
+    divide, 
+    modulus, 
+    even, 
+    odd 
+};
